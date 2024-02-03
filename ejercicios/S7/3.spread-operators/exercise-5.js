699c8cea32a4d7568d649c08f7e2b191656bc770
ivanCeballos-upgradeHub <ivan.ceballos@bootcamp-upgrade.com>
2024-02-03 14:24:20 +0100

    Ejercicios S7 resueltos

---
+++ ejercicios/S7/3.spread-operators/exercise-5.js
@@ -0,0 +1,9 @@
+/**
+ * Dado el siguiente array. Crear una copia de él eliminando la posición 2 pero sin editar el array inicial.
+ * De nuevo, usando spread operatos.
+ */
+
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+const elementToRemove = 2;
+const colorsCopy = [...colors.slice(0, elementToRemove), ...colors.slice(elementToRemove + 1)];
